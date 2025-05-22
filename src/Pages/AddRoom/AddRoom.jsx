266fb1c706fea601065c759ae5540112f6da3810
code-shams <code.meshams@gmail.com>
2025-05-22 13:04:38 +0600

    Add Post UI completed

--- src/Pages/AddRoom/AddRoom.jsx
+++ src/Pages/AddRoom/AddRoom.jsx
@@ -1,9 +1,130 @@
-import React from 'react'
+import React, { use } from "react";
+import { AuthContext } from "../../Contexts/AuthProvider";
+import { FaChevronDown } from "react-icons/fa6";
 
 const AddRoom = () => {
-  return (
-    <div>AddRoom</div>
-  )
-}
+    const { user } = use(AuthContext);
+    const handleAddPost = (e) => {
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        const formObj = Object.fromEntries(formData.entries());
+        formObj.name = user.displayName;
+        formObj.email = user.email;
+        formObj.likeCount = 0;
+        formObj.availability = "Available";
+        console.log(formObj);
+    };
+    return (
+        <div className="max-w-[1515px] w-11/12 mx-auto my-10 sec-font">
+            <div className="rounded-lg p-2 pt-0 md:p-5 md:pt-0 shadow-lg group hover:shadow-xl transition-all duration-500 border border-lime-100">
+                <h1 className="text-center text-lg sm:text-2xl md:text-3xl font-extrabold pri-font bg-black text-lime-200 rounded-b-full md:w-1/2 lg:w-1/3 mx-auto group-hover:scale-105 group-hover:p-2 transition-all duration-500 opacity-80 group-hover:opacity-100 overflow-hidden">
+                    New Post
+                </h1>
 
-export default AddRoom
+                {/* Form  */}
+
+                <div>
+                    <form
+                        onSubmit={handleAddPost}
+                        className="pt-2 md:pt-5 pb-0"
+                    >
+                        {/* Username and email */}
+                        <div className="flex flex-col sm:flex-row gap-2 sm:gap-4 mb-2 md:mb-6">
+                            <input
+                                type="text"
+                                name="name"
+                                className="mt-1 block w-full sm:w-1/2 rounded-md border border-slate-300 bg-gray-50 px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-black focus:outline-none focus:ring-1 focus:ring-black sm:text-sm"
+                                disabled
+                                value={user.displayName}
+                            />
+                            <input
+                                type="text"
+                                name="email"
+                                className="mt-1 block w-full sm:w-1/2 rounded-md border border-slate-300 bg-gray-50 px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-black focus:outline-none focus:ring-1 focus:ring-black sm:text-sm"
+                                disabled
+                                value={user.email}
+                            />
+                        </div>
+
+                        {/* title and location */}
+                        <div className="flex flex-col sm:flex-row gap-2 sm:gap-4">
+                            <input
+                                type="text"
+                                name="title"
+                                className="mt-1 block w-full sm:w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-black focus:outline-none focus:ring-1 focus:ring-black sm:text-sm"
+                                placeholder="Title"
+                            />
+                            <input
+                                type="text"
+                                name="location"
+                                className="mt-1 block w-full sm:w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-black focus:outline-none focus:ring-1 focus:ring-black sm:text-sm"
+                                placeholder="Location"
+                            />
+                        </div>
+
+                        {/* rent and room type */}
+                        <div className="flex flex-col sm:flex-row gap-2 sm:gap-4 my-2 md:my-6">
+                            <input
+                                type="text"
+                                name="rentAmount"
+                                className="mt-1 block w-full sm:w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-black focus:outline-none focus:ring-1 focus:ring-black sm:text-sm"
+                                placeholder="Rent"
+                            />
+                            <input
+                                type="text"
+                                name="roomType"
+                                className="mt-1 block w-full sm:w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-black focus:outline-none focus:ring-1 focus:ring-black sm:text-sm"
+                                placeholder="Room type"
+                            />
+                        </div>
+
+                        {/* preferences and contact */}
+                        <div className="flex flex-col sm:flex-row gap-2 sm:gap-4 my-2 md:my-6">
+                            <input
+                                type="text"
+                                name="preferences"
+                                className="mt-1 block w-full sm:w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-black focus:outline-none focus:ring-1 focus:ring-black sm:text-sm"
+                                placeholder="Preferences"
+                            />
+                            <input
+                                type="text"
+                                name="contactInfo"
+                                className="mt-1 block w-full sm:w-1/2 rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-black focus:outline-none focus:ring-1 focus:ring-black sm:text-sm"
+                                placeholder="Contact"
+                            />
+                        </div>
+                        <div className="my-2 sm:my-6">
+                            <input
+                                type="text"
+                                name="photo"
+                                className="mt-1 block w-full rounded-md border border-slate-300 bg-white px-3 py-4 placeholder-slate-400 shadow-sm placeholder:font-semibold placeholder:text-gray-500 focus:border-black focus:outline-none focus:ring-1 focus:ring-black sm:text-sm"
+                                placeholder="Photo Url (Optional)"
+                            />
+                        </div>
+                        <div>
+                            <textarea
+                                name="details"
+                                cols="30"
+                                rows="10"
+                                className="border-2 border-slate-300 h-40 w-full resize-none rounded-md p-5 font-semibold text-gray-500 focus:outline-black"
+                            >
+                                Details
+                            </textarea>
+                        </div>
+                        <div className="text-center ">
+                            <button
+                                type="submit"
+                                className="btn btn-md w-full sm:w-auto px-12 md:text-lg bg-black text-white hover:scale-110 rounded-full transition-all duration-300"
+                            >
+                                Add
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AddRoom;
--- src/Pages/Home/FeaturedPost/FeaturedPost.jsx
+++ src/Pages/Home/FeaturedPost/FeaturedPost.jsx
@@ -12,7 +12,6 @@ const FeaturedPost = ({ posts }) => {
                 <h1 className="text-center text-lg sm:text-2xl md:text-3xl font-extrabold pri-font bg-black text-lime-200 rounded-b-full md:w-1/2 lg:w-1/3 mx-auto group-hover:scale-105 group-hover:p-2 transition-all duration-500 opacity-80 hover:opacity-100 overflow-hidden">
                     Featured Posts
                 </h1>
-                {/* <div className="w-1/3 mx-auto bg-rose-500 pt-[2px]"></div> */}
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 mt-5 relative">
                     {posts.map((post, index) => (
                         <PostCard
